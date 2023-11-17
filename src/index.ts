940777f80a0e01c1c361bc736ce881c58ce8cdf8
hoonjoo94 <hoonjoo94@naver.com>
2023-11-18 01:38:29 +0900

    prisma-client 배포를 위한 index.ts 및 index.d.ts 파일 세팅

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export * from "../prisma/prisma";
+import { PrismaClient } from "@prisma/client";
+import { Context } from "./index.d";
+
+const bambooPrisma = new PrismaClient();
+
+export const createContext = async (ctx: any): Promise<Context> => {
+  ctx.callbackWaitsForEmptyEventLoop = false;
+
+  return { ...ctx, bambooPrisma };
+};
