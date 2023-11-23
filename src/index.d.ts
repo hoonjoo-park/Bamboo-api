df58784117887937015374ae56f964bd37e3365f
hoonjoo94 <hoonjoo94@naver.com>
2023-11-23 21:33:38 +0900

    배포용 Index 파일들 제거

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export * from ".prisma/client/index.d";
-
-export interface BambooPrisma {
-  bambooPrisma: PrismaClient;
-}
-
-export function getBambooPrisma(): Promise<BambooPrisma>;
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { BambooPrisma } from "./index.d";
-
-const bambooPrisma = new PrismaClient();
-
-export const getBambooPrisma = async (): Promise<BambooPrisma> => {
-  return { bambooPrisma };
-};
