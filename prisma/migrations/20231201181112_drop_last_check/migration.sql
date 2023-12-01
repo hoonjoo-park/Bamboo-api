4a95b41cdc291872253e58321b555867dba094f2
hoonjoo94 <hoonjoo94@naver.com>
2023-12-02 03:15:39 +0900

    refactor database structure for chat feature

---
+++ prisma/migrations/20231201181112_drop_last_check/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastCheck` on the `UserChatRoom` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `UserChatRoom` DROP COLUMN `lastCheck`;
