d6192f8e8fc4849eae50ad04a22ce37293a698fd
hoonjoo94 <hoonjoo94@naver.com>
2023-12-02 19:35:12 +0900

    GET: /chat 리턴 타입 수정

--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import prisma from "../../prisma/prisma";
 import { authUser } from "../middlewares/auth-helper";
+import { Message } from "@prisma/client";
 
 export const chatUrl = "/chat";
 export const chatRouter = Router();
@@ -47,32 +48,55 @@ chatRouter.post("/", authUser, async (req: Request, res: Response) => {
   const { chatRoomId, message } = req.body;
   const userId = req.userId;
 
-  const chatRoom = await prisma.chatRoom.findUnique({
-    where: {
-      id: chatRoomId,
-    },
-  });
+  try {
+    const chatRoom = await prisma.chatRoom.findUnique({
+      where: {
+        id: chatRoomId,
+      },
+    });
 
-  if (!chatRoom) {
-    res.status(404).json({ error: "ChatRoom not found" });
+    if (!chatRoom) {
+      res.status(404).json({ error: "ChatRoom not found" });
+    }
+
+    const chatMessage = await prisma.message.create({
+      data: {
+        senderId: userId,
+        chatRoomId,
+        content: message,
+      },
+      include: {
+        sender: {
+          select: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    if (!chatMessage) {
+      res.status(500).json({ error: "cannot create message" });
+    }
+
+    const messageToReturn = {
+      id: chatMessage.id,
+      chatRoomId: chatMessage.chatRoomId,
+      content: chatMessage.content,
+      createdAt: chatMessage.createdAt,
+      userProfile: chatMessage.sender.profile,
+    };
+
+    await prisma.userChatRoom.update({
+      where: {
+        userId_chatRoomId: { userId, chatRoomId },
+      },
+      data: {
+        lastReadMessageId: messageToReturn.id,
+      },
+    });
+
+    res.status(200).json(messageToReturn);
+  } catch (error) {
+    res.status(500).json({ error: "error while creating chat message" });
   }
-
-  const chatMessage = await prisma.message.create({
-    data: {
-      senderId: userId,
-      chatRoomId,
-      content: message,
-    },
-  });
-
-  await prisma.userChatRoom.update({
-    where: {
-      userId_chatRoomId: { userId, chatRoomId },
-    },
-    data: {
-      lastReadMessageId: message.id,
-    },
-  });
-
-  res.status(200).json(chatMessage);
 });
